090d11c09d9bc7d248857f4e5047c6761a6c042f
Kotchamon  Jadeekan <70263535+JKotchamon@users.noreply.github.com>
2025-10-27 04:26:26 +1100

    add features Maze

---
+++ RE/Maze/solve.py
@@ -0,0 +1,19 @@
+import base64
+
+payload = b'IiIudGkgZ25pbm51ciBvZiBkYWV0c25pIGVkb2Mgc2lodCB0bmlycCB0c3VKICMKIiBuMDF0cHlyY24zX3Qwbl9zMV80Nl8zczRiJ1ZVQyIgPSBnYWxmCi5lcmVoIHRoZ2lyIHNpIGdhbGYgZWhUICMKIHJlZW5pZ25lIGVzcmV2ZXIgLGVub2QgbGxlVyAjIiIi'
+
+def unleash(data):
+    try:
+
+        decoded = base64.b64decode(data)
+        unscrambled = decoded.decode('utf-8')[::-1]
+        
+        # Run the secret code
+        print(unscrambled)
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    print("Running the secret task... done.")
+    unleash(payload)
